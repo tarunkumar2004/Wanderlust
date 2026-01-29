0d741735c92f67dfb103f0b34e0523102716a457
Tarun Kumar <tarunbhardwaj27112004@gmail.com>
2026-01-29 22:42:24 +0530

    Create user.js

---
+++ routes/user.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../Models/user.js");
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
+
+const userController = require("../controllers/users.js");
+
+router
+  .route("/signup")
+  .get( userController.renderSignupForm )
+  .post( userController.signup )
+
+router
+  .route("/login")
+  .get( userController.renderLogin )
+  .post( saveRedirectUrl, passport.authenticate("local",{failureRedirect: "/login",failureFlash: true,}),wrapAsync(userController.login));
+
+router.get("/logout", userController.logout );
+
+module.exports = router;
