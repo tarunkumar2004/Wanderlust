4d1e924caace2c6259971cee66d8988ea1b9e400
Tarun Kumar <tarunbhardwaj27112004@gmail.com>
2026-01-29 23:13:49 +0530

    Create listing.js

---
+++ Models/listing.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+const review = require("./review.js");
+const Schema = mongoose.Schema;
+const Review = require("../Models/review.js");
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  image: {
+    url: String,
+    filename: String,
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    }
+  ],
+
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+  geometry: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+  }
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if(listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
+
+
