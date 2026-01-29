08b83c011a6e75913a92e4d19d3954f925f5e7e7
Tarun Kumar <tarunbhardwaj27112004@gmail.com>
2026-01-29 22:41:51 +0530

    Create review.js

---
+++ routes/review.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router({mergeParams: true});
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const Listing = require("../Models/listing.js");
+const review = require("../Models/review.js");
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js");
+
+const reviewController = require("../controllers/review.js");
+
+// Reviews
+//Post Review Route
+
+router.post("/",isLoggedIn, validateReview, wrapAsync(reviewController.createReview));
+
+//Delete Review Route
+
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, wrapAsync(reviewController.destroyReview));
+
+module.exports = router;
